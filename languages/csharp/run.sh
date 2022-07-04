ae1534c8ac71c5d018495b6f5b6fd6e0cab3dcea
Tomio <mail@tomio.fun>
2022-07-04 22:18:38 +0800

    Update run.sh

--- languages/csharp/run.sh
+++ languages/csharp/run.sh
@@ -2,5 +2,5 @@
 
 printf %s"\n" "$1" > program.csx
 echo "$2" > .input
-shift
-DOTNET_CLI_HOME="/tmp/DOTNET_CLI_HOME" dotnet script program.csx < .input
+shift 2
+DOTNET_CLI_HOME="/tmp/DOTNET_CLI_HOME" dotnet script program.csx "$@" < .input
