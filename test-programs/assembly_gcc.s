18e14e88914b2f2ff7e00b8308fb13762ddda390
Tomio <mail@tomio.fun>
2022-07-06 06:31:28 +0000

    ci: fix gcc

--- test-programs/assembly_gcc.s
+++ test-programs/assembly_gcc.s
@@ -1,13 +1,19 @@
-SYS_write = 4
-STDOUT = 1
-.data
-hello:
-.string "Hello, World!"
-.globl main
-main:
-movl $SYS_write,%eax
-movl $STDOUT,%ebx
-movl $hello,%ecx
-movl $13,%edx
-int $0x80
-ret
+.global _start
+
+.text
+
+_start:
+    # write (1, msj, 13)
+    mov $1, %rax            # system call 1 is write
+    mov $1, %rdi            # file handler 1 is stdout
+    mov $message, %rsi      # address of string to output
+    mov $13, %rdx           # number of bytes
+    syscall
+
+    # exit(0)
+    mov $60, %rax           # system call 60 is exit
+    xor %rdi, %rdi          # we want to return code 0
+    syscall
+
+message:
+    .ascii "Hello, World!\n"
