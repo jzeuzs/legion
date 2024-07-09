67c86fa67d06fe821998cd72fa6af9cab508759c
Jez <75403863+jzeuzs@users.noreply.github.com>
2024-07-09 23:13:24 +0800

    fix: go

--- languages/go/run.sh
+++ languages/go/run.sh
@@ -4,5 +4,5 @@ export GOCACHE=/tmp/go-cache
 printf %s"\n" "$1" > program.go
 echo "$2" > .input
 shift 2
-go build program.go
+CGO_ENABLED=0 GOOS=linux go build program.go -o ./program
 ./program "$@" < .input
